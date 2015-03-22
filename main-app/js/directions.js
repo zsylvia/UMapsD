b44b4becdbd0d8c318c42ffc5a803cda5732c6be
Lutcikaur <lutcikaur@gmail.com>
2015-03-22 13:23:46 -0400

    more accurate debug info, slightly more robust. still needs some data validation.

--- main-app/js/directions.js
+++ main-app/js/directions.js
@@ -8,16 +8,6 @@ Stair: bldg_???_flr_???_st_???
 Elevator: bldg_???_flr_???_el_???
 */
 
-/*
-	graph convention ; 
-	marker.data(GlobalStrings.ID/TYPE/BUILDING/FLOOR) 
-	marker.attr("cx"/"cy");
-		   0    1   2   3   4  5 
-	path : bldg_???_flr_???_rm_??? <-> bldg_???_flr_???_rm_??? 
-	
-*/
-var advanced = true;
-
 function findAngle2(p1,p2,p3){
 	var a1 = p2.cx - p1.cx;
 	var a2 = p2.cy - p1.cy;
@@ -29,6 +19,14 @@ function findAngle2(p1,p2,p3){
 	return result;
 }
 
+/*
+displayPath(path){} takes in an array of strings, that follow the naming convention above.
+anything after rm_ is basically ignored, its not important for displaying the path.
+as long as the strings can be found in the different xxxMaps that exist, it is valid input.
+
+If path contains an invalid string, unhappyness will occur.
+*/
+
 function displayPath(path){
 	document.getElementById('displayPath').innerHTML = "";
 	var addDirection = function( txt ) {
@@ -39,16 +37,15 @@ function displayPath(path){
 		var i = 2;
 		var queue = [];
 		var names = [];
+		var advanced = true;
 
-		//add in required
+		//add in required initial nodes
 		names.push(path[0]);
 		queue.push(names[0].split("_"));
 		names.push(path[1]);
 		queue.push(names[1].split("_"));
 		names.push(path[2]);
 		queue.push(names[2].split("_"));
-
-		console.log(names);
 
 		while (++i < path.length){
 			var obs = [];
@@ -65,13 +62,12 @@ function displayPath(path){
 					case ("brm") : obs[j] = bathroomMensMap[table.names[j]].value.attrs; break;
 					case ("brw") : obs[j] = bathroomWomensMap.table[names[j]].value.attrs; break;
 					case ("error") :
-					default : console.log("Error! Type not Implemented! Aborting advnced directions!"); advanced=false; break;
+					default : console.log("Error! Direction type not Implemented! Aborting advnced directions for this step!"); advanced=false; break;
 				}
 			}
-			//console.log(obs[0], obs[1], obs[2]);
+			advanced = true;
 			degree=findAngle2(obs[0], obs[1], obs[2]);
-			console.log("Angle of " + names[0] + " " + names[1] + " " + names[2]);
-			console.log(findAngle2(obs[0], obs[1], obs[2]));
+			console.log("Angle of " + names[0] + " " + names[1] + " " + names[2] + " : " + findAngle2(obs[0], obs[1], obs[2]));
 
 			if (advanced){
 				var dir = (degree < 30 && degree >= -30)?"continue strait ": 
@@ -83,28 +79,31 @@ function displayPath(path){
 				(degree < -120 && degree >= -150)?"take a sharp left ": 
 				(degree < -150 || degree >= 150)?"make a uturn (This should never happen) ":
 				"error";
+				//should sharp left and sharp right be adjusted so they're basically uturns?
 			}
 
 			switch(queue[2][4]){
 				case ("rm") : addDirection("temp; Go " + (advanced === true?dir:"") + "through the room"); break;
 				case ("dr") : addDirection("temp; Go " + (advanced === true?dir:"") + "through the door"); break;
 				case ("hw") : addDirection("temp; Continue " + (advanced === true?dir:"") + "down the hallway"); break;
-				case ("pw") : addDirection("temp; Continue " + (advanced === true?dir:"") + "down the [pw]"); break;
-				case ("st") : addDirection("temp; Continue " + (advanced === true?dir:"") + "UPORDOWN? the stairwell"); break;
-				case ("ev") : addDirection("temp; Continue " + (advanced === true?dir:"") + "down the [ev]"); break;
+				case ("pw") : addDirection("temp; Continue " + (advanced === true?dir:"") + "down the pathway"); break;
+				case ("st") : addDirection("temp; Continue " + (advanced === true?dir:"") + "up/down the stairwell"); break;
+				case ("ev") : addDirection("temp; Continue " + (advanced === true?dir:"") + "up/down the elevator"); break;
 				case ("brm") : addDirection("temp; Continue " + (advanced === true?dir:"") + "down the [boysroom]"); break;
 				case ("brw") : addDirection("temp; Continue " + (advanced === true?dir:"") + "down the [girlsroom]"); break;
-				default : addDirection("error"); break;
+				default : addDirection("Error computing this step of pathway."); 
+					//put in aditional error logging.
+					break;
 			}
 
-			//at the end...
+			// shift set of 3 nodes in prep for next loop.
 			queue.push(path[i].split("_"));
 			names.push(path[i]);
-			console.log(names[3]);
 			queue.shift();
 			names.shift();
 		}
 	} else {
+		//else : you want directions to something so fucking close I'm not going to give you words to find it.
 		addDirection("You're so close! It should be through a nearby door.");
 	}
 	$("#displayPath").append( '</ul>' );
